176e727604f77bd8eb0005846db12fd6cb06ef6e
arcadyrajeev <bitmasteratj@gmail.com>
2025-10-29 22:24:05 +0530

    new commit

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,12 +2,14 @@ import AddonsSection from '@/components/sections/AddonSection'
 import FaqSection from '@/components/sections/FAQ'
 import PricingSection from '@/components/sections/PricingHero'
 import React from 'react'
+import faq from '@/data/top-faqs.json'
 
 const PricingPage = () => {
   return (
     <div>
       <PricingSection />
       <AddonsSection />
+      <FaqSection faqs={faq} />
     </div>
   )
 }
