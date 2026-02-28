300ca92a1ec3f6c2b59a2ca29fd92605dfcff338
Rahul Rao <rahulrao@Rahuls-MacBook-Pro.local>
2026-03-01 03:22:03 +0530

    google code for data collections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
+import Script from 'next/script'
 
 import './globals.css'
 import Navbar from '@/components/ui/navbar'
@@ -66,6 +67,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${nunitoSans.variable} overflow-x-hidden`}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4B37EK5RBC"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4B37EK5RBC');
+          `}
+        </Script>
+      </head>
       <body className="antialiased background-black text-white overflow-x-hidden">
         <SmoothScrollProvider>
           <Navbar />
